eb42b5c5a8b6a9f8a1807e78ca7574ee8b5f4a25
ChuckNunchak <54897002+ChuckNunchak@users.noreply.github.com>
2020-02-19 15:20:45 +0300

    Add files via upload

---
+++ Currency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Currency
+    {
+        public string PreviousUrl { get; set; }
+
+        public Dictionary<string, Valute> Valute { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            string response;
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+            Currency currency = JsonConvert.DeserializeObject<Currency>(response);
+
+
+            Console.WriteLine("{0} - {1}", currency.Valute["AUD"].Name, currency.Valute["AUD"].Value);
+            Console.ReadLine();
+
+        }
+    }
+}
